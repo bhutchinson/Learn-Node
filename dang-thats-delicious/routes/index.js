47f10a691bfa254b03cc8b027a91106a91459ae0
Bryan Hutchinson <bryan@kuali.co>
2017-10-03 12:01:18 -0400

    lesson 7

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,22 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const bryan = {name: 'Bryan', age: 46, cool: false };
-  // res.send('Hey! It works!');
-  // res.json(bryan);
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-    name: 'Bryan',
-    dog: req.query.dog
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
